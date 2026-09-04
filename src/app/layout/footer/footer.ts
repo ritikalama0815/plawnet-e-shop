6e30d7d2326007fea4acb03c9cb0e9c4291143cd
Ritika Lama <ritikatheeng2002@gmail.com>
2026-09-03 22:00:59 -0500

    header and footer

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {}
+export class Footer {
+  readonly year = new Date().getFullYear();
+}
